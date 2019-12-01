9586ac02335cb2725ebe209697951c98508bb856
Burbage Stacey <rstacey06@gmail.com>
2019-12-01 16:32:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
-  # write code here
+  
+  class TriangleError < StandardError
+  end
+  
+  attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize (size)
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end
 end
